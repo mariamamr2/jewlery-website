99e1740f7066567c6e9972bcf7ce42529229b47a
mariamamr2 <mariamamr778@gmail.com>
2022-08-29 20:42:49 +0200

    mjewelery website

---
+++ bracelet.js
@@ -0,0 +1,10 @@
+function viewmore(){
+    document.getElementById("images2").style.display="grid";
+    document.getElementById("button1").style.display="none";
+    document.getElementById("button2").style.display="block";
+}
+function viewless(){
+    document.getElementById("images2").style.display="none";
+    document.getElementById("button1").style.display="block";
+    document.getElementById("button2").style.display="none";
+}
---
+++ home.js
@@ -0,0 +1,81 @@
+
+$(".salesimg1").hover(
+    function(){
+    $(".titleprice1").show(2000);
+    }, function() {
+        $(".titleprice1").hide(2000);
+    }
+);
+
+$(".salesimg2").hover(
+    function(){
+    $(".titleprice2").show(2000);
+    }, function() {
+        $(".titleprice2").hide(2000);
+    }
+);
+
+
+$(".salesimg3").hover(
+    function(){
+    $(".titleprice3").show(2000);
+    }, function() {
+        $(".titleprice3").hide(2000);
+    }
+);
+
+
+$(".salesimg4").hover(
+    function(){
+    $(".titleprice4").show(2000);
+    }, function() {
+        $(".titleprice4").hide(2000);
+    }
+);
+
+
+$(".salesimg5").hover(
+    function(){
+    $(".titleprice5").show(2000);
+    }, function() {
+        $(".titleprice5").hide(2000);
+    }
+);
+
+
+$(".salesimg6").hover(
+    function(){
+    $(".titleprice6").show(2000);
+    }, function() {
+        $(".titleprice6").hide(2000);
+    }
+);
+
+
+$(".salesimg7").hover(
+    function(){
+    $(".titleprice7").show(2000);
+    }, function() {
+        $(".titleprice7").hide(2000);
+    }
+);
+
+
+$(".salesimg8").hover(
+    function(){
+    $(".titleprice8").show(2000);
+    }, function() {
+        $(".titleprice8").hide(2000);
+    }
+);
+
+
+$(".salesimg9").hover(
+    function(){
+    $(".titleprice9").show(2000);
+    }, function() {
+        $(".titleprice9").hide(2000);
+    }
+);
+
+
